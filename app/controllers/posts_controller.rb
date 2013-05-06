b07afa1f53096adf9cfa52bb0d9c595eef215dcd
Laurence Joseph Smith <laurence@sourceless.org>
2013-05-06 18:04:54 +0100

    Add routes, add auth to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,54 +1,41 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.json
+  http_basic_authenticate_with  :username => ENV['USERNAME'], :password => ENV['PASSWORD'], :except => :show
   def index
     @posts = Post.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @posts }
     end
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
     @post = Post.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @post }
     end
   end
 
-  # GET /posts/new
-  # GET /posts/new.json
   def new
     @post = Post.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @post }
     end
   end
 
-  # GET /posts/1/edit
   def edit
     @post = Post.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
     @post = Post.new(params[:post])
 
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render json: @post, status: :created, location: @post }
       else
         format.html { render action: "new" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end

07546234b475ef17c4ec2065a45e16aed408f35e
Laurence Joseph Smith <laurence@sourceless.org>
2013-05-06 18:32:42 +0100

    Add model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :rendered_content, :title
+
+  validates :title, presence:true, uniqueness:true
+  validates :content, presence:true
+  validates :rendered_content, presence:true
 end
